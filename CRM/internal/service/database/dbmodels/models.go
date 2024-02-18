42768e80b8826dd16438c1b15d01043bbea45ed0
Luke Kemner <37083547+kemnerluke@users.noreply.github.com>
2024-02-18 09:16:13 -0500

    now connected to DB

--- CRM/internal/service/database/dbmodels/models.go
+++ CRM/internal/service/database/dbmodels/models.go
@@ -1,4 +1,4 @@
-package database
+package main
 
 type Customer struct {
 	FirstName   string `json:"first_name"`
---
+++ CRM/internal/service/database/postgres/customer.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/google/uuid"
+
+func  GetCustomers(memberKey uuid.UUID) (records []dbmodels.Customer, err error) {
+
+	return nil,nil
+}
