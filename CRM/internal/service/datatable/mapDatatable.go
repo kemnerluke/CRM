cda515c9a796223ab5236a8500e2878531c5ebc8
Luke Kemner <37083547+kemnerluke@users.noreply.github.com>
2024-02-21 08:54:37 -0500

    added theader and t row structs

--- CRM/internal/service/datatable/mapDatatable.go
+++ CRM/internal/service/datatable/mapDatatable.go
@@ -3,11 +3,38 @@ package datatable
 import "CRM/internal/service/database/dbmodels"
 
 type DataTable struct {
+	THeader THeader
+	TRow    TRow
 
 }
 
+type THeader struct{
+	Columns []DataTableColumn
 
-func mapRecordsToDatatable([]dbmodels.Customer) DataTable {
+}
+
+type TRow struct {
+	Rows []DataTableRow
+
+
+}
+
+type DataTableColumn struct {
+	Name string
+	Show bool
+
+}
+
+type DataTableRow struct {
+	Cells DataTableCells
+
+}
+
+type DataTableCells struct{
+
+}
+
+func MapRecordsToDatatable([]dbmodels.Customer) DataTable {
 
 	var dataTable = DataTable{}
 
